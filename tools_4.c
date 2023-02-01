e4f1030b6f776b3d01c525987197e8a3b32fd912
idouni <idouni@student.1337.ma>
2023-02-01 14:26:56 +0100

    push_swap

--- tools_4.c
+++ tools_4.c
@@ -15,9 +15,17 @@
 t_data  find_the_lis(t_data data, int l)
 {
     int *index_arr;
+    // int *lis_values;
 
     index_arr = NULL;
-    index_arr = (int *)h_malloc(data, (l * sizeof(int)), index_arr);
+    index_arr = (int *)h_malloc(data, (l * sizeof(int)) + 1, index_arr);
+    // while()
     
+
+
+
+
+    // lis_values = (int *)h_malloc(data, (max * sizeof(int)) + 1, lis_values);
+    // free(index_arr);
     return (data);
 }
