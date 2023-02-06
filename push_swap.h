8b3536296ddc9bcf4a04ca79fe84b196591192f2
idouni <idouni@student.1337.ma>
2023-02-06 13:39:57 +0100

    push_swap

--- push_swap.h
+++ push_swap.h
@@ -67,6 +67,7 @@ t_data	put_min_on_top(t_data data);
 t_data	sort_small_qt(t_data data);
 t_data	rotate_both_stacks(t_data data, t_list *best_mem);
 t_data	normal_instructions_stacks(t_data data, t_list *best_mem);
+t_list	*moves(t_data data, t_list *n);
 t_data	sort_5(t_data data);
 t_data	empty_b(t_data data, int l);
 int		mid_value(t_data data);
--- tools_6.c
+++ tools_6.c
@@ -15,36 +15,44 @@
 t_data	moves_required(t_data data)
 {
 	t_list	*n;
-	double	half_size;
-	int		l2;
 
-	l2 = ft_lstsize(data.stack_a) / 2;
-	half_size = (ft_lstsize(data.stack_b) / 2);
 	n = data.stack_b;
 	while (n)
 	{
-		n->mv_b = moves_to_be_on_top(data.stack_b, n, half_size);
-		n->mv_a = moves_on_a(data, n, l2);
-		n->mv_rr = 0;
-		n->mv_rrr = 0;
-		if (n->mv_a < 0 && n->mv_b < 0)
-		{
-			n->mv_rrr = abs(n->mv_a - n->mv_b);
-			n->mv_b += n->mv_rrr;
-			n->mv_a += n->mv_rrr;
-		}
-		else if (n->mv_a > 0 && n->mv_b > 0)
-		{
-			n->mv_rr = abs(n->mv_a - n->mv_b);
-			n->mv_b -= n->mv_rr;
-			n->mv_a -= n->mv_rr;
-		}
+		n = moves(data, n);
 		n->mv_required = abs(n->mv_rr) + abs(n->mv_rrr) + \
 		abs(n->mv_a) + abs(n->mv_b) + 1;
 		n = n->next;
 	}
 	return (data);
 }
+
+t_list	*moves(t_data data, t_list *n)
+{
+	double	half_size;
+	int		l2;
+
+	l2 = ft_lstsize(data.stack_a) / 2;
+	half_size = (ft_lstsize(data.stack_b) / 2);
+	n->mv_b = moves_to_be_on_top(data.stack_b, n, half_size);
+	n->mv_a = moves_on_a(data, n, l2);
+	n->mv_rr = 0;
+	n->mv_rrr = 0;
+	if (n->mv_a < 0 && n->mv_b < 0)
+	{
+		n->mv_rrr = abs(n->mv_a - n->mv_b);
+		n->mv_b += n->mv_rrr;
+		n->mv_a += n->mv_rrr;
+	}
+	else if (n->mv_a > 0 && n->mv_b > 0)
+	{
+		n->mv_rr = abs(n->mv_a - n->mv_b);
+		n->mv_b -= n->mv_rr;
+		n->mv_a -= n->mv_rr;
+	}
+	return (n);
+}
+	
 
 int	moves_to_be_on_top(t_list *stack, t_list *node, double half_size)
 {
