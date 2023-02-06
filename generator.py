9ed8cb661734d1329bca0e34b71f4da691e6f23b
idouni <idouni@student.1337.ma>
2023-02-06 13:53:24 +0100

    push_swap

--- generator.py
+++ generator.py
@@ -1,6 +1,6 @@
 import random
 
-nums = list(range(0, 99)) # list of integers from 1 to 99
+nums = list(range(0, 9999)) # list of integers from 1 to 99
                            # adjust this boundaries to fit your needs
 random.shuffle(nums)
 print(*nums)
