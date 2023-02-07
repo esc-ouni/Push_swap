bfb8db9b29e403fc6475e1e4656b948ba5fffa26
idouni <idouni@student.1337.ma>
2023-02-07 13:46:41 +0100

    push_swap

--- checker_bonus.c
+++ checker_bonus.c
@@ -29,7 +29,10 @@ int	main(int argc, char *argv[])
 		data = get_instruction(data, s);
 		if (s)
 			free(s);
+		s = get_next_line(0);
 	}
+	if (s)
+		free(s);
 	if (check_if_sorted(data.stack_a) && !ft_lstsize(data.stack_b))
 		ft_printf("OK\n");
 	else
@@ -39,27 +42,29 @@ int	main(int argc, char *argv[])
 
 t_data	get_instruction(t_data data, char *s)
 {
-	if (ft_strcmp(s, "ra"))
+	if (!ft_strcmp(s, "ra\n"))
 		data = ch_rotate_a(data);
-	else if (ft_strcmp(s, "rra"))
+	else if (!ft_strcmp(s, "rra\n"))
 		data = ch_reverse_rotate_a(data);
-	else if (ft_strcmp(s, "rb"))
+	else if (!ft_strcmp(s, "rb\n"))
 		data = ch_rotate_b(data);
-	else if (ft_strcmp(s, "rrb"))
+	else if (!ft_strcmp(s, "rrb\n"))
 		data = ch_reverse_rotate_b(data);
-	else if (ft_strcmp(s, "sa"))
+	else if (!ft_strcmp(s, "sa\n"))
 		data = ch_swap_a(data);
-	else if (ft_strcmp(s, "sb"))
+	else if (!ft_strcmp(s, "sb\n"))
 		data = ch_swap_b(data);
-	else if (ft_strcmp(s, "ss"))
+	else if (!ft_strcmp(s, "ss\n"))
 		data = ch_swap_s(data);
-	else if (ft_strcmp(s, "pa"))
+	else if (!ft_strcmp(s, "pa\n"))
 		data = ch_push_a(data);
-	else if (ft_strcmp(s, "pb"))
+	else if (!ft_strcmp(s, "pb\n"))
 		data = ch_push_b(data);
-	else if (ft_strcmp(s, "rr"))
+	else if (!ft_strcmp(s, "rr\n"))
 		data = ch_rotate_r(data);
-	else if (ft_strcmp(s, "rrr"))
+	else if (!ft_strcmp(s, "rrr\n"))
 		data = ch_reverse_rotate_r(data);
+	else
+		ft_exit_with_error(data);
 	return (data);
 }
